bf68fe63382a3896b89c4554208b644b8f32adc1
marco_carioti <marcocarioti.mc@gmail.com>
2021-10-13 13:10:31 +0200

    Last

--- app/src/main/java/com/example/segnapunteggio/SecondFragment.kt
+++ app/src/main/java/com/example/segnapunteggio/SecondFragment.kt
@@ -6,11 +6,13 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.findFragment
+import androidx.navigation.fragment.navArgs
 import com.example.segnapunteggio.databinding.FragmentFirstBinding
 import com.example.segnapunteggio.databinding.FragmentHomeBinding
 import com.example.segnapunteggio.databinding.FragmentSecondBinding
 
 class SecondFragment : Fragment() {
+    val args:SecondFragmentArgs by navArgs()
     private var _binding: FragmentSecondBinding? = null
     private val binding get() = _binding!!
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -19,14 +21,13 @@ class SecondFragment : Fragment() {
 
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
-        return inflater.inflate(R.layout.fragment_second, container, false)
         _binding= FragmentSecondBinding.inflate(inflater,container,false)
         return binding.root
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        binding.risultatoFinale.text= args.punteggioCasa
+        binding.risultatoFinale2.text=args.punteggioTrasferta
     }
-
-
 }

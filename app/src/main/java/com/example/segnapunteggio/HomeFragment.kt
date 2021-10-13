fbdbc1bf2b351caeaed6baf3622aaad1b1bb8c90
marco_carioti <marcocarioti.mc@gmail.com>
2021-10-13 09:57:13 +0200

    Modifica

--- app/src/main/java/com/example/segnapunteggio/HomeFragment.kt
+++ app/src/main/java/com/example/segnapunteggio/HomeFragment.kt
@@ -1,10 +1,12 @@
 package com.example.segnapunteggio
 
 import android.os.Bundle
+import android.text.Layout
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.fragment.findNavController
 import com.example.segnapunteggio.databinding.FragmentHomeBinding
 
 
@@ -15,11 +17,20 @@ class HomeFragment : Fragment() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
+
         }
+
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         _binding= FragmentHomeBinding.inflate(inflater,container,false)
         return binding.root
+        super.onCreateView(inflater, container, savedInstanceState)
+        binding.button1.setOnClickListener(){
+            val action = HomeFragmentDirections.actionHomeFragmentToFirstFragment()
+            findNavController().navigate(action)
+        }
+
+
     }
     }
 
